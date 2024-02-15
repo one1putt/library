bab86b93c7190f6c93caf9eb7efcb0642771b2dd
one1putt <klericksonmn@gmail.com>
2024-02-15 17:09:05 -0600

    reorganized js and added comments

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const myLibrary = [] //this will be an array of Book objects
+const addButton = document.querySelector('.add-book')
+const sideBar = document.querySelector('.side-bar')
+addButton.addEventListener('click', enterForm)
+sideBar.addEventListener('submit', (e) => addBookToLibrary(e))
+const library = document.querySelector('.library')
+
+// This is Book constructor
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -14,8 +22,9 @@ function Book(title, author, pages, read) {
     }
 }
 
-const myLibrary = []
-
+// populate the database with three books so we have some data to work with
+// at this stage, we are not storing this in a database as that 
+// functionality will come later 
 let book = new Book('Catch-22', 'Joseph Heller', 453, false)
 myLibrary.push(book)
 book = new Book('Shoe Dog', 'Phil Knight', 402, true)
@@ -23,27 +32,18 @@ myLibrary.push(book)
 book = new Book('Killing the witches', "Bill O'Reily", 293, true)
 myLibrary.push(book)
 
-function addBookToLibrary(e) {
-    e.preventDefault()
-    book = new Book(document.forms[0][0].value, 
-                    document.forms[0][1].value,
-                    document.forms[0][2].value, 
-                    document.forms[0][3].value)
-    myLibrary.push(book)
-    sideBar.removeChild(sideBar.lastChild)   
-    refreshLibrary()
-}
-
-const library = document.querySelector('.library')
-
+// whenever we change the database, we run this function to update the
+// library section of the screen with the changes
 function refreshLibrary() {
     library.innerHTML = ''
 
     myLibrary.forEach( (book, index) => {
         let readNotRead = book.read ? 'Read' : 'Not read'
+        // we create a new div to hold the card
         let cardDiv = document.createElement('div')
         cardDiv.className = 'book-card'
         cardDiv.setAttribute('data-index', index)
+        // then we create the card elements that we add to the card
         let card = `<div class='title'><h4>"${book.title}"</h4><button class='delete'>X</button></div>
         <table>
             <tr>
@@ -59,9 +59,11 @@ function refreshLibrary() {
                 <td>${readNotRead}</td>
             </tr>
         </table>`
-        cardDiv.innerHTML = card
-        library.appendChild(cardDiv)
+        cardDiv.innerHTML = card //add the elements to the new card div
+        library.appendChild(cardDiv) //then add to our index html
     })
+    
+    // I could not get it working with adding the addEventListeners in the loop above. so i added these two extra loops to add the addEventListeners after the fact
     const deleteCard = document.querySelectorAll('.delete')
     for (card of deleteCard) {
         card.addEventListener('click', (e) => {
@@ -76,18 +78,33 @@ function refreshLibrary() {
     }
 }
 
+// executed when the save button is pushed to add a new book
+function addBookToLibrary(e) {
+    e.preventDefault()
+    book = new Book(document.forms[0][0].value, 
+                    document.forms[0][1].value,
+                    document.forms[0][2].value, 
+                    document.forms[0][3].value)
+    myLibrary.push(book)
+    sideBar.removeChild(sideBar.lastChild)   
+    refreshLibrary()
+}
+
+
 function changeReadStatus(e) {
     let index = e.target.parentElement.parentElement.parentElement.parentElement.parentElement.getAttribute('data-index')
     myLibrary[index].read ? myLibrary[index].read = false : myLibrary[index].read = true
     refreshLibrary()
 }
 
+// to delete a book from the database
 function removeCard(e) {
     let index = e.target.parentElement.parentElement.getAttribute('data-index')
     myLibrary.splice(index, 1)
     refreshLibrary()
 }
 
+// this function gets executed when the add book button is pushed. It creates the whole html for the form and displays it in the sideBar
 function enterForm() {
     const addBookForm = document.createElement('div')
     let formHTML = `<form action="">
@@ -113,10 +130,6 @@ function enterForm() {
     sideBar.appendChild(addBookForm)
 }
 
-const addButton = document.querySelector('.add-book')
-const sideBar = document.querySelector('.side-bar')
-addButton.addEventListener('click', enterForm)
-sideBar.addEventListener('submit', (e) => addBookToLibrary(e))
 refreshLibrary()
 
 
